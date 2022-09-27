4ac2b38119bb01f33193372df5602a013473da94
Ellen Uttley <113264368+ellenuttley@users.noreply.github.com>
2022-09-27 21:28:46 +0100

    Create rannum.py
    A very small "project" for a random number generator game.

---
+++ rannum.py
@@ -0,0 +1,25 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print("Sorry, guess again! Too low.")
+        elif guess > random_number:
+            print ("Sorry, guess again. Answer too high!")
+    
+    print(f"Yayy! {random_number} is correct!")
+        
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != 'c':
+        guess = random.rantint(low,high)
+        
+
+
+guess(10)
+ 
